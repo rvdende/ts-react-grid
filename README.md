4f31af3ed227b92cbe2550fcae1c0d8cdc0eb192
Rouan van der Ende <rouan.vanderende@iotnxt.com>
2019-11-12 13:16:27 +0200

    v1.0.1 update Readme

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# ts-react-grid
-typescript react grid component
+WARNING: This is a very new package.
+
+![Alt text](ts-react-grid_preview.png?raw=true "Preview")
+
+```
+npm i ts-react-grid -s
+```
+
+Import and use the grid:
+
+```ts
+import { Grid } from "ts-react-grid";
+```
+
+
+
+```html
+<Grid columnCount={3} gutterWidth={5} gutterHeight={5} >
+    <div style={{ background: "#58D08F", padding: 5 }}>box1</div>
+    <div style={{ background: "#58D0D9", padding: 5 }}>box2</div>
+    <div style={{ border: "1px solid red" }}>box3</div>
+    <div style={{ border: "1px solid blue" }}>box4</div>
+    <div style={{ border: "1px solid green" }}>box5</div>
+</Grid>
+
+```
+
+## Documentation
+
+
+We do not handle responsive width for you, but you can easily toggle columnCount based on `window.innerWidth` yourself.
+
+`columnCount` (not optional) This is the amount of columns you want in your grid.
+
+`gutterWidth` (optional) px gutter width between columns (default: 10)
+
+`gutterHeight` (optional) px gutter height between rows (default: 10)
+
+
+## Changelog:
+
+```
+v1.0.1 update README.md
+v1.0.0 initial npm publish
+```
+
+## Notes
+
+NPM link: https://www.npmjs.com/package/ts-react-grid   
+GITHUB link: https://github.com/rvdende/ts-react-grid
---
+++ src/tsreactgrid.tsx
@@ -0,0 +1,84 @@
+import * as React from "react";
+import { SizeMe } from 'react-sizeme'
+
+interface Props {
+    children: any;
+    columnCount?: number;
+}
+
+interface State {
+    columnCount: number;
+    gutterWidth: number;
+    gutterHeight: number;
+}
+
+export class Grid extends React.Component<Props, State> {
+    state = {
+        columnCount: 4,
+        gutterWidth: 10,
+        gutterHeight: 10
+    }
+
+    static getDerivedStateFromProps(props: Props, state: State) {
+        // var newstate = {}
+        // if (props.columnCount) { newstate.columnCount }
+        return props;
+    }
+
+    render() {
+
+        return (
+            <SizeMe>
+                {({ size }) => {
+                    var gridcalc = this.calculate(size);
+                    return (<div style={{}}>
+                        {gridcalc}
+                        <div style={{ clear: "both" }} />
+                    </div>)
+                }}
+            </SizeMe>
+        )
+    }
+
+    calculate(size: { width: number | null, height: number | null }) {
+
+        if (!size.width) { return []; }
+
+        var rows: any = [];
+
+        //calc number of rows
+        var numrows = Math.ceil(this.props.children.length / this.state.columnCount);
+
+        var currow: any = [];
+        for (var c = 0; c < this.props.children.length; c++) {
+            var rownum = Math.floor(c / this.state.columnCount);
+            var column = c % this.state.columnCount
+            var lastcolumn = (column == (this.state.columnCount - 1)) ? true : false;
+            var lastrow = (rownum == (numrows - 1)) ? true : false;
+
+            if (((c / this.state.columnCount) % 1) != 0) {
+                currow.push(<div key={c + "margin"} style={{ width: this.state.gutterWidth, height: "100%" }} />)
+            }
+
+            var width = (size.width - (this.state.gutterWidth * (this.state.columnCount - 1))) / this.state.columnCount
+
+            var style = {
+                ...this.props.children[c].props.style,
+                ...{ width }
+            };
+
+            //console.log("column", column)
+            currow.push(React.cloneElement(this.props.children[c], { style, key: c }))
+
+            if ((c == this.props.children.length - 1) || (column == (this.state.columnCount - 1))) {
+                //console.log("end of row " + rownum + "(" + c + ")")
+                var style: any = { display: "flex", flexDirection: "row" }
+                if (lastrow == false) { style.marginBottom = this.state.gutterHeight }
+                rows.push(<div key={Math.random()} style={style}>{currow}</div>);
+                currow = [];
+            }
+        }
+
+        return rows;
+    }
+}
